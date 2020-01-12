7934445c95d21c61d9bd69c3e446403575fc8698
Zavier Jack <zavierjack1@gmail.com>
2020-01-12 13:14:00 -0500

    added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
   AppComponent,
-  PostCreateComponent
+  PostCreateComponent, 
+  HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     BrowserAnimationsModule, 
     MatInputModule, 
     MatCardModule, 
-    MatButtonModule
+    MatButtonModule, 
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
