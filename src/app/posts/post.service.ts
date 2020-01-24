fde7348b2747e8ce560adfeffa4089681d7171a7
Zavier Jack <zavierjack1@gmail.com>
2020-01-23 22:01:42 -0500

    added nodeServerAddress

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,7 +1,7 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs' //Subject is similar to an EventEmitter
-import { map } from 'rxjs/operators' //
+import { map } from 'rxjs/operators'
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
@@ -10,13 +10,16 @@ export class PostService{
     private posts: Post[]= [];
     private postUpdated = new Subject<Post[]>();
     private httpClient: HttpClient;
+    //the nodeServerAddress is the IP address of the node server relative to the client. (using the docker name wont work because angular runs on the CLIENT)
+    ///0.0.0.0 wont work on work computer if running node on linux academy because the server isnt at localhost
+    private nodeServerAddress: string = 'http://0.0.0.0:8081';
 
     constructor( httpClient: HttpClient, private router: Router){
         this.httpClient = httpClient;
     }
 
     getPosts(){
-        this.httpClient.get<{message: string, posts: any}>('http://0.0.0.0:8081/api/posts')
+        this.httpClient.get<{message: string, posts: any}>(this.nodeServerAddress+'/api/posts')
             .pipe(map((postData) => {
                 return postData.posts.map(post => {
                     return {
@@ -33,7 +36,7 @@ export class PostService{
     }
 
     getPost(id: string){
-        return this.httpClient.get<{_id: string, title: string, content: string}>('http://0.0.0.0:8081/api/posts/'+id);
+        return this.httpClient.get<{_id: string, title: string, content: string}>(this.nodeServerAddress+'/api/posts/'+id);
     }
 
     getPostUpdatedListener(){
@@ -43,7 +46,7 @@ export class PostService{
     addPost(post: Post){
         //remember this Express server happens to live at 0.0.0.0 we'll want to 
         //pass in a env variable at run time with the public IP of the Express server
-        this.httpClient.post<{message:string, postId:string}>('http://0.0.0.0:8081/api/posts', post)
+        this.httpClient.post<{message:string, postId:string}>(this.nodeServerAddress+'/api/posts', post)
         .subscribe((responseData) =>{
             console.log(responseData.message);
             post.id = responseData.postId;
@@ -54,7 +57,7 @@ export class PostService{
     }
 
     deletePost(postId: string){
-        this.httpClient.delete<{message: string}>('http://0.0.0.0:8081/api/posts/'+postId)
+        this.httpClient.delete<{message: string}>(this.nodeServerAddress+'/api/posts/'+postId)
         .subscribe((responseData) => {
             console.log(responseData.message);
             this.posts = this.posts.filter(post => post.id !== postId);
@@ -63,7 +66,7 @@ export class PostService{
     }
 
     updatePost(post: Post){
-        this.httpClient.put<{message: string}>('http://0.0.0.0:8081/api/posts/'+post.id, post)
+        this.httpClient.put<{message: string}>(this.nodeServerAddress+'/api/posts/'+post.id, post)
             .subscribe((responseData) => {
                 console.log(responseData.message);
                 const updatedIndex = this.posts.findIndex(p => p.id === post.id);
