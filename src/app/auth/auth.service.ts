adb2181f320d597ef82ebec955f575c4856b5e19
Zavier Jack <zavierjack1@gmail.com>
2020-02-15 08:38:43 -0500

    improve the UI Header to relfect authentication status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,18 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { AuthData } from './auth-data.model';
+import { Subject } from "rxjs"
 
+/*
+injectable providedIn: root tells Angular where this service is and only creates one instance of it.
+similarly, you couldve added this calss to the Providers array in app.module.ts and gotten the same effect.
+*/
 @Injectable({
     providedIn: "root"
 })
 export class AuthService{
     private token: string;
+    private authStatusListener = new Subject<boolean>();
     private nodeServerAddress: string = environment.nodeUrl;
     
     constructor(private httpClient: HttpClient){}
 
     getToken(){
         return this.token;
+    }
+
+    getAuthStatusListener(){
+        return this.authStatusListener.asObservable();
     }
 
     createUser(email: string, password: string){
@@ -29,6 +39,7 @@ export class AuthService{
         this.httpClient.post<{token: string}>(this.nodeServerAddress+'/api/user/login', authData)
             .subscribe(response => {
                 this.token = response.token;
+                this.authStatusListener.next(true);
             })
     }
 }
