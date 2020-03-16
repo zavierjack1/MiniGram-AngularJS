98dc2e042ce99300c200fcda4f9275cb82bab589
Zavier Jack <zavierjack1@gmail.com>
2020-03-15 20:58:21 -0400

    added new enviornment variable setups

---
+++ src/app/environments/environment.local.ts
@@ -0,0 +1,5 @@
+export const environment = {  
+    production: false,
+    nodeUrl: 'http://0.0.0.0:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+};
+
