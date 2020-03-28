66f561f6220b95206f9d33e1932447918e4ddfc0
Zavier Jack <zavierjack1@gmail.com>
2020-03-28 14:30:13 -0400

    added dynamic environment file config

--- src/app/environments/environment.local.ts
+++ src/app/environments/environment.local.ts
@@ -1,5 +1,5 @@
 export const environment = {  
     production: false,
-    nodeUrl: 'http://0.0.0.0:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+    nodeUrl: 'http://10.0.0.86:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
 };
 
--- src/app/environments/environment.test.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {  
-    production: false,
-    nodeUrl: 'http://test:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
-};
-
