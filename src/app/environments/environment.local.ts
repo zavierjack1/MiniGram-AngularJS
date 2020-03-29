befcf332c246ebaf1f4acb8188c5d01a893c0490
zavier jack <zavierjack1@gmail.com>
2020-03-29 01:10:35 +0000

    latest working version, no need to build on Docker build

--- src/app/environments/environment.local.ts
+++ src/app/environments/environment.local.ts
@@ -1,5 +1,5 @@
 export const environment = {  
-    production: false,
+    production: true,
     nodeUrl: 'http://10.0.0.86:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
 };
 
