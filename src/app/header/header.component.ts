d1ecf5f8f250102672a14b63e0ff132c61fc19ec
zavier jack <zavierjack1@gmail.com>
2020-04-26 20:45:12 +0000

    added roboto font as default, added welcome message

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,14 +11,18 @@ export class HeaderComponent implements OnInit, OnDestroy{
     public userIsAuthenticated = false;
     private authStatusSub: Subscription;
     constructor(private authService:AuthService){}
-
+    userData: {userId: string, email: string, admin: boolean}; 
     ngOnInit(){
         this.authStatusSub = this.authService
             .getAuthStatusListener()
             .subscribe(isAuthenticated => {
                 this.userIsAuthenticated = isAuthenticated
+                if (isAuthenticated){
+                    this.userData = this.authService.getUserData();
+                }
             })
         this.userIsAuthenticated = this.authService.getIsAuthenticated();
+        this.userData = this.authService.getUserData();
     }
     ngOnDestroy(){
         this.authStatusSub.unsubscribe();
