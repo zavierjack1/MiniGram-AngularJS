c1e1859e9da424ab0c9a524f5501f7085293ba86
zavier jack <zavierjack1@gmail.com>
2020-04-29 02:04:13 +0000

    added environment.prod.ts

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {  
+    production: true,
+    nodeUrl: 'http://3.22.34.189:80' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+};
